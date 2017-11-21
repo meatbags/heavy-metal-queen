fb5bf208b4dd02a431a605d51581fd6e3a367d77
Xavier Burrow <jxburrow@gmail.com>
2017-11-21 21:42:57 +1100

    styling

--- index.php
+++ index.php
@@ -2,6 +2,48 @@
 get_header();
 ?>
 
+<div class='loading-screen'>
+  <div class='loading-screen__inner'>
+    <div class='loading-message'>
+      LOADING
+    </div>
+    <div class='loading-bar'>
+      <div class='loading-bar__inner'></div>
+    </div>
+  </div>
+</div>
+
+<div class='nav'>
+  <div class='nav-item' data-target='#menu-chapters'>
+    CHAPTERS
+  </div>
+  <div class='nav-item' data-target='#menu-about'>
+    ABOUT
+  </div>
+</div>
+
+<div class='status'>
+  <div class='status-item'>
+    <span id='page-no'>1</span>
+  </div>
+</div>
+
+<div id='menu-chapters' class='menu'>
+  <div class='menu__inner'>
+    <div class='menu__inner-pad'>&nbsp;</div>
+    CHAPTERS
+  </div>
+</div>
+<div id='menu-about' class='menu'>
+  <div class='menu__inner'>
+    <div class='menu__inner-pad'>&nbsp;</div>
+    ABOUT<br />
+    instagram<br />
+    facebook<br />
+    etsy, etc.
+  </div>
+</div>
+
 <div class='title-page'>
   <div class='layer background'>
     <img src='<?php echo get_template_directory_uri() . '/lib/images/queen_background.jpg'; ?>' />
@@ -12,7 +54,13 @@ get_header();
   <div class='layer'>
     <img src='<?php echo get_template_directory_uri() . '/lib/images/queen_title.png'; ?>' />
   </div>
+  <div class='layer'>
+    <div class='title-prompt'>
+    </div>
+  </div>
 </div>
+
+<div class='prompt pre text-large'>&#9660;</div>
 
 <?php
 $query = new WP_Query(array(
